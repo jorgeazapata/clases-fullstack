448deae0eb8ebfae7254cd8cb3d417b49e415a2b
andres zapata <jorgeandresz8910@hotmail.com>
2021-08-07 22:49:46 -0500

    funcionalidades js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.onload = inicio;
+
+	function inicio() {
+  var nombre ="bienvenido al curso fullstack"
+  
+    alert("hola "+nombre)
+	}
---
+++ js/pendiente.js
@@ -0,0 +1,5 @@
+window.onload = inicio;
+
+function inicio(){
+   var nombre = prompt("Ingrese su nombre")
+   alert("Hola " + nombre)
